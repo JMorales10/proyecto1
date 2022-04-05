592f52e4845c8e8f250b8e4a0c35d973bb758f73
usuario3jesus <jesusmoralescanete@gmail.com>
2022-04-05 17:29:14 +0200

    .

--- jesusMorales/src/Prueba1/Secundario.java
+++ jesusMorales/src/Prueba1/Secundario.java
@@ -3,7 +3,7 @@ package Prueba1;
 public class Secundario {
 
 	public static void main(String[] args) {
-		System.out.println("Modifico el repositorio después de clonarlo");
+		System.out.println("Modifico el repositorio después de clonarlo2");
 
 	}
 
