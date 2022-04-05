e1dc1cd568e15bd81ac5c81649d70caf4dabb5cf
usuario3jesus <jesusmoralescanete@gmail.com>
2022-04-05 17:31:56 +0200

    Valido la clase prueba 2

--- jesusMorales/src/Prueba1/Secundario.java
+++ jesusMorales/src/Prueba1/Secundario.java
@@ -3,7 +3,7 @@ package Prueba1;
 public class Secundario {
 
 	public static void main(String[] args) {
-		System.out.println("Modifico el repositorio después de clonarlo2");
+		System.out.println("Modifico el repositorio después de clonarlo");
 
 	}
 
